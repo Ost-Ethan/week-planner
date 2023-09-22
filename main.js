8b4878ee26dd33f27fbc31cca1d9ace1c8937cc8
Tyler Stahl <tstahl488@gmail.com>
2023-09-22 18:06:54 +0000

    renderEntry function and data modeling

--- main.js
+++ main.js
@@ -11,11 +11,35 @@ $newEventModal.addEventListener('click', handleModalButton);
 
 function handleModalButton(event) {
   if (event.target.getAttribute('id') === 'cancel-button') {
+    $eventForm.reset();
     $newEventModal.setAttribute('class', 'hidden');
   }
   if (event.target.getAttribute('id') === 'confirm-button') {
+    const newEventData = {
+      time: $eventTime.value,
+      day: $eventDay.value,
+      event: $eventNotes
+    };
+    renderEntry(newEventData);
+
     $newEventModal.setAttribute('class', 'hidden');
   }
 }
 
-// const newEventData = {}
+const data = {
+  storedEvents: []
+};
+
+const $eventTime = document.querySelector('#time-of-event');
+const $eventDay = document.querySelector('#day-of-event');
+const $eventNotes = document.querySelector('#event-infomration');
+const $eventForm = document.querySelector('#new-event-form');
+
+const $tBody = document.querySelector('tbody');
+
+function renderEntry(newEventData) {
+  const $tdTime = document.createElement('td');
+  const $tdDay = documentt.createElemnt('td');
+  const $tdInfo = docutment.createElement('td');
+
+}
